b47384476eddeaa56331ea1ac23424fcc87ec219
theo-f-marechal <73380218+theo-f-marechal@users.noreply.github.com>
2020-10-24 09:57:56 +0200

    add lunch msg

---
+++ src/main/java/com/example/msg/LunchMsg.java
@@ -0,0 +1,10 @@
+package com.example.msg;
+
+import java.io.Serializable;
+
+public class LunchMsg implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public void lunch(){
+
+    }
+}
