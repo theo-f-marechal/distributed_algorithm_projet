e516f2d9027e3f32d7f47ba06821aa09b0462f95
theo-f-marechal <73380218+theo-f-marechal@users.noreply.github.com>
2020-11-07 23:11:36 +0100

    first version of the WriterReader

--- src/main/java/com/example/WriterReader.java
+++ src/main/java/com/example/WriterReader.java
@@ -43,106 +43,112 @@ public class WriterReader extends UntypedAbstractActor {
     }
 
     public int read() {
-            r++;
-            ArrayList<Integer> ballotR = new ArrayList<>();
-            ballotR.add(r);
-            ActorRef auxip1 = createAuxiliary(); // create the auxiliary process n°1
+        r++;
+        ArrayList<Integer> ballotR = new ArrayList<>();
+        ballotR.add(r);
+        ActorRef auxip1 = createAuxiliary(); // create the auxiliary process n°1
 
-            ReadMsg messageR = new ReadMsg(ballotR, auxip1);
+        ReadMsg messageR = new ReadMsg(ballotR, auxip1);
 
-            for (ActorRef i : processes.references) { //send messages to all processes
-                if (i == self())
-                    continue;
-                i.tell(messageR, self()); // send a message with the auxiliary process ref
-            }
+        for (ActorRef i : processes.references) { //send messages to all processes
+            if (i == self())
+                continue;
+            i.tell(messageR, self()); // send a message with the auxiliary process ref
+        }
 
-            Future<Object> future1 = Patterns.ask(auxip1, new StartAnsweringMsg(), timeout);
-            //wait
-            try {
-                // wait for the auxiliary process to answer
-                AuxiliaryReadAnswerMsg resultR = (AuxiliaryReadAnswerMsg) Await.result(future1, timeout.duration());
+        Future<Object> future1 = Patterns.ask(auxip1, new StartAnsweringMsg(), timeout);
+        //wait
+        try {
+            // wait for the auxiliary process to answer
+            AuxiliaryReadAnswerMsg resultR = (AuxiliaryReadAnswerMsg) Await.result(future1, timeout.duration());
 
-                ArrayList<Integer> ballotW = resultR.ballot; // recover the result sent by auxip1 [vm;tm]
-                system.stop(auxip1); // close the now useless auxiliary process
-                ActorRef auxip2 = createAuxiliary(); // create the auxiliary process n°2
+            ArrayList<Integer> ballotW = resultR.ballot; // recover the result sent by auxip1 [vm;tm]
+            system.stop(auxip1); // close the now useless auxiliary process
+            ActorRef auxip2 = createAuxiliary(); // create the auxiliary process n°2
 
-                WriteMsg messageW = new WriteMsg(ballotW, auxip2);
-
-                for (ActorRef i : processes.references) { //send msg to all process
-                    if (i == self())
-                        continue;
-                    i.tell(messageW, self());
-                }
-                // wait
-                Future<Object> future2 = Patterns.ask(auxip1, new StartAnsweringMsg(), timeout);
-                AuxiliaryWriteAnswerMsg resultW = (AuxiliaryWriteAnswerMsg) Await.result(future2, timeout.duration());
-
-                system.stop(auxip2);
-
-                if (resultW.ballot) //check that all the msg were received
-                    return ballotW.get(0);
-                return -1; // return vm
-
-            } catch (TimeoutException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-    }
-
-    public boolean write(int value){
-            r++;
-            ArrayList<Integer> ballotR = new ArrayList<>();
-            ballotR.add(r);
-            ActorRef auxip1 = createAuxiliary(); // create the auxiliary process n°1
-
-            ReadMsg messageR = new ReadMsg(ballotR, auxip1);
+            WriteMsg messageW = new WriteMsg(ballotW, auxip2);
 
             for (ActorRef i : processes.references) { //send msg to all process
                 if (i == self())
                     continue;
-                i.tell(messageR, self()); //send as auxip1
+                i.tell(messageW, self());
+            }
+            // wait
+            Future<Object> future2 = Patterns.ask(auxip1, new StartAnsweringMsg(), timeout);
+            AuxiliaryWriteAnswerMsg resultW = (AuxiliaryWriteAnswerMsg) Await.result(future2, timeout.duration());
+
+            system.stop(auxip2);
+
+            if (resultW.ballot) //check that all the msg were received
+                return ballotW.get(0);
+            return -1; // return vm
+
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public boolean write(int value){
+        r++;
+        ArrayList<Integer> ballotR = new ArrayList<>();
+        ballotR.add(r);
+        ActorRef auxip1 = createAuxiliary(); // create the auxiliary process n°1
+
+        ReadMsg messageR = new ReadMsg(ballotR, auxip1);
+
+        for (ActorRef i : processes.references) { //send msg to all process
+            if (i == self())
+                continue;
+            i.tell(messageR, self()); //send as auxip1
+        }
+
+        Future<Object> future1 = Patterns.ask(auxip1, new StartAnsweringMsg(), timeout);
+        try {
+            AuxiliaryReadAnswerMsg resultR = (AuxiliaryReadAnswerMsg) Await.result(future1, timeout.duration());
+
+            t = resultR.ballot.get(1) + 1; // tm + 1
+            ArrayList<Integer> ballotW = new ArrayList<>();
+            ballotW.add(value); ballotW.add(t);
+            system.stop(auxip1); // close the now useless auxiliary process
+            ActorRef auxip2 = createAuxiliary(); // create the auxiliary process n°2
+
+            WriteMsg messageW = new WriteMsg(ballotW, auxip2);
+
+            for (ActorRef i : processes.references) { //send msg to all process
+                if (i == self())
+                    continue;
+                i.tell(messageW, self());
             }
 
-            Future<Object> future1 = Patterns.ask(auxip1, new StartAnsweringMsg(), timeout);
-            try {
-                AuxiliaryReadAnswerMsg resultR = (AuxiliaryReadAnswerMsg) Await.result(future1, timeout.duration());
+            //wait
+            Future<Object> future2 = Patterns.ask(auxip1, new StartAnsweringMsg(), timeout);
+            AuxiliaryWriteAnswerMsg resultW = (AuxiliaryWriteAnswerMsg) Await.result(future2, timeout.duration());
+            system.stop(auxip2);
 
-                t = resultR.ballot.get(1) + 1; // tm + 1
-                ArrayList<Integer> ballotW = new ArrayList<>();
-                ballotW.add(value); ballotW.add(t);
-                system.stop(auxip1); // close the now useless auxiliary process
-                ActorRef auxip2 = createAuxiliary(); // create the auxiliary process n°2
-
-                WriteMsg messageW = new WriteMsg(ballotW, auxip2);
-
-                for (ActorRef i : processes.references) { //send msg to all process
-                    if (i == self())
-                        continue;
-                    i.tell(messageW, self());
-                }
-
-                //wait
-                Future<Object> future2 = Patterns.ask(auxip1, new StartAnsweringMsg(), timeout);
-                AuxiliaryWriteAnswerMsg resultW = (AuxiliaryWriteAnswerMsg) Await.result(future2, timeout.duration());
-                system.stop(auxip2);
-
-                return resultW.ballot;
-            } catch (TimeoutException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            return resultW.ballot;
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Override
     public void onReceive(Object message) throws Throwable {
-        if(){
+        if(message instanceof UpdateMsg){
             int value = 2;
             boolean succes = write(value);
             log.info(self().path().name() + " wrote " + value + " (almost) everywhere with succes? " + succes);
+            UpdateMsg msg1 = new UpdateMsg(r,t,false);
+            parent.tell(msg1, self());
             int read_value = read();
             log.info(self().path().name() + " read " + read_value + " (almost) everywhere");
+            UpdateMsg msg1 = new UpdateMsg(r,t,false);
+            parent.tell(msg1, self());
         }
 
     }
